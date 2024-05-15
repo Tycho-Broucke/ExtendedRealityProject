a948041f1785d88a9a96cf97ffe3ed83f72cca15
Tomas Vermeulen <tomas.vermeulen@student.kuleuven.be>
2024-05-15 17:47:05 +0200

    Cube fix

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -12,6 +12,8 @@ public class Cube : MonoBehaviour
         {
             Debug.LogError("Object does not have a MeshRenderer component.");
         }
+
+        cubeRenderer.material.color = new Color(50, 50, 50);
     }
 
     public void ChangeColorRandomly()
@@ -27,13 +29,13 @@ public class Cube : MonoBehaviour
     {
         float yPosition = Mathf.Lerp(2f, 1.27f, amount);
 
-        transform.position = new Vector3(transform.position.x, yPosition, transform.position.z);
+        transform.position = new Vector3(transform.position.x, yPosition + 1.4f, transform.position.z);
     }
 
     public void MoveLeftRight(float amount)
     {
         float xPosition = Mathf.Lerp(0.44f, -0.44f, amount);
 
-        transform.position = new Vector3(xPosition, transform.position.y, transform.position.z);
+        transform.position = new Vector3(transform.position.x, transform.position.y, xPosition - 3);
     }
 }
