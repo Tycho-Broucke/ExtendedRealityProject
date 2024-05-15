ea1268047e52070d700507aabc9dda744ccdc7d9
Tomas Vermeulen <tomas.vermeulen@student.kuleuven.be>
2024-05-15 15:24:03 +0200

    Move left/right

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WateringCan : MonoBehaviour
+public class Cube : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -18,8 +18,15 @@ public class WateringCan : MonoBehaviour
 
     public void MoveUpDown(float amount)
     {
-        float newPosition = Mathf.Lerp(0.2f, 3f, amount);
+        float newPosition = Mathf.Lerp(3f, 0.27f, amount);
 
         transform.position = new Vector3(transform.position.x, newPosition, transform.position.z);
+    }
+
+    public void MoveLeftRight(float amount)
+    {
+        float newPosition = Mathf.Lerp(0.44f, -0.44f, amount);
+
+        transform.position = new Vector3(newPosition, transform.position.y, transform.position.z);
     }
 }
